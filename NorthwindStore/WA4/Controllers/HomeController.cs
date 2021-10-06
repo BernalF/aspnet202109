116d5cdf009ffb03fdd474c20b6220146e8bbb47
Bernal Fernandez <bfernandez@its.cr>
2021-10-06 09:35:40 -0600

    lab2

---
+++ NorthwindStore/WA4/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Store.Data;
+using WA4.Models;
+using WA4.Extensions;
+
+namespace WA4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> logger;
+        private NWContext db;
+
+        public HomeController(ILogger<HomeController> logger, NWContext db)
+        {
+            this.logger = logger;
+            this.db = db;
+        }
+
+        public IActionResult Index(IEnumerable<CategoryProductsViewModel> vm)
+        {
+            vm = from p in db.Products.Include(p => p.Category).ToList()
+                group p by p.Category?.CategoryName ?? "Sin Categoría"
+                into categoryProducts
+                select new CategoryProductsViewModel
+                {
+                    CategoryName = categoryProducts.Key, Items = categoryProducts.ToList()
+                };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string filter)
+        {
+            IEnumerable<CategoryProductsViewModel> vm = 
+                from p in db.Products.Include(p => p.Category).ToList()
+                where p.ProductName.Contains(filter)
+                group p by p.Category?.CategoryName ?? "Sin Categoría"
+                into categoryProducts
+                select new CategoryProductsViewModel
+                {
+                    CategoryName = categoryProducts.Key, Items = categoryProducts.ToList()
+                };
+
+            return View(vm);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+        public IActionResult IndexPartial(int? id)
+        {
+            var isAjax = Request.IsAjaxRequest();
+
+            if (id != null)
+            {
+                return PartialView("ProductPartial", db.Products.Where(p => p.ProductId == id).SingleOrDefault());
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        public IActionResult IndexViewComponent(int? id)
+        {
+            var isAjax = Request.IsAjaxRequest();
+
+            if (id != null)
+            {
+                return ViewComponent("Product", new { id });
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ NorthwindStore/WA4/ViewModels/CategoryProductsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Northwind.Store.Model;
+
+namespace WA4.Models
+{
+    public class CategoryProductsViewModel
+    {
+        public string CategoryName { get; set; }
+        public List<Product> Items { get; set; }
+
+        public string Filter { get; set; }
+
+        public CategoryProductsViewModel()
+        {
+            Items = new List<Product>();
+        }
+    }
+}
