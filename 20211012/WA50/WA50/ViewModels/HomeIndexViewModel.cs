9fce33ca47b7ac9a0eba37440048ffcb1a8b9750
Gilberto Bermúdez Garro <gbermude@outlook.com>
2021-10-12 16:30:53 -0600

    Actualización

--- 20211012/WA50/WA50/ViewModels/HomeIndexViewModel.cs
+++ 20211012/WA50/WA50/ViewModels/HomeIndexViewModel.cs
@@ -1,11 +1,12 @@
 ﻿using Northwind.Store.Model;
 using System.Collections.Generic;
+using X.PagedList;
 
 namespace WA50.ViewModels
 {
     public class HomeIndexViewModel
     {
         public string Filter { get; set; }
-        public List<Product> Products { get; set; }
+        public IPagedList<Product> Products { get; set; }
     }
 }
