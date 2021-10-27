76b514fe9188cebcb4deedb88eb58a4d84025ad4
Bernal Fernandez <bfernandez@its.cr>
2021-10-26 19:02:38 -0600

    quiz 10 26 2021

---
+++ quiz_10262021/ToDoQuiz/Models/ToDo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace ToDoQuiz.Models
+{
+    [Table("ToDo")]
+    public partial class ToDo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        [Column(TypeName = "date")]
+        public DateTime? Due { get; set; }
+    }
+}
